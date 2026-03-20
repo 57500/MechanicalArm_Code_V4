1c81fa2c1a4202427154b67e3af307efcc5c71f0
1 <1>
2026-03-20 22:23:15 +0800

    Null

--- Task/Uart_Task.cpp
+++ Task/Uart_Task.cpp
@@ -15,59 +15,63 @@ void Uart_Task(void *pvParameters)
     {
         if (xQueueReceive(Uart_Queue_H, Date, portMAX_DELAY) != pdTRUE) continue;
 
-        if (Current_Task != NULL)
+        // if (Current_Task != NULL)
+        // {
+        //     Stop_Flag = true;
+        //
+        //     if (xSemaphoreTake(Stop_Semaphore_H, pdMS_TO_TICKS(200)) != pdTRUE)
+        //     {
+        //         nb.Clear();
+        //         nb.Emergency_Stop();
+        //         Current_Task = NULL;
+        //         HAL_TIM_Base_Start_IT(&htim1);
+        //         HAL_UART_Transmit(&huart1,(uint8_t*)"StopSemaphore Miss",sizeof("StopSemaphore Miss"),100);
+        //     }
+        // }
+        // Stop_Flag = false;
+        //
+        // if (strncmp((char*)Date, "PTP:", 4) == 0)
+        // {
+        //     char *token;
+        //     int count = 0;
+        //     float temp[6];
+        //
+        //     // 以此逗号 "," 为分隔符获取第一个数据
+        //     token = strtok((char*)Date+4, ",");
+        //
+        //     while(token != NULL && count < 6)
+        //     {
+        //         //将字符串转为浮点数
+        //         temp[count] = atof(token);
+        //         count++;
+        //
+        //         //以此逗号 "," 为分隔符获取下一个数据
+        //         token = strtok(NULL, ",");
+        //     }
+        //
+        //     if (count == 6)
+        //     {
+        //         Coordinates_Pose target;
+        //         target.x=temp[0];
+        //         target.y=temp[1];
+        //         target.z=temp[2];
+        //         target.alpha=temp[3];
+        //         target.beta=temp[4];
+        //         target.gamma=temp[5];
+        //
+        //         nb.UpDate_Current_Angle_Rad();
+        //         nb.UpDate_Current_CP();
+        //         nb.UpDate_Target_CP(target);
+        //         nb.UpDate_S_Curve_Profile();
+        //
+        //         Current_Task=PTP_Handle;
+        //         HAL_TIM_Base_Start_IT(&htim1);
+        //         xTaskNotifyGive(Current_Task);
+        //     }
+        // }
+         if (strncmp((char*)Date, "PAD:", 4) == 0)
         {
-            Stop_Flag = true;
-
-            if (xSemaphoreTake(Stop_Semaphore_H, pdMS_TO_TICKS(200)) != pdTRUE)
-            {
-                nb.Clear();
-                nb.Emergency_Stop();
-                Current_Task = NULL;
-                HAL_TIM_Base_Start_IT(&htim1);
-                HAL_UART_Transmit(&huart1,(uint8_t*)"StopSemaphore Miss",sizeof("StopSemaphore Miss"),100);
-            }
-        }
-        Stop_Flag = false;
-
-        if (strncmp((char*)Date, "PTP:", 4) == 0)
-        {
-            char *token;
-            int count = 0;
-            float temp[6];
-
-            // 以此逗号 "," 为分隔符获取第一个数据
-            token = strtok((char*)Date+4, ",");
-
-            while(token != NULL && count < 6)
-            {
-                //将字符串转为浮点数
-                temp[count] = atof(token);
-                count++;
-
-                //以此逗号 "," 为分隔符获取下一个数据
-                token = strtok(NULL, ",");
-            }
-
-            if (count == 6)
-            {
-                Coordinates_Pose target;
-                target.x=temp[0];
-                target.y=temp[1];
-                target.z=temp[2];
-                target.alpha=temp[3];
-                target.beta=temp[4];
-                target.gamma=temp[5];
-
-                nb.UpDate_Current_Angle_Rad();
-                nb.UpDate_Current_CP();
-                nb.UpDate_Target_CP(target);
-                nb.UpDate_S_Curve_Profile();
-
-                Current_Task=PTP_Handle;
-                HAL_TIM_Base_Start_IT(&htim1);
-                xTaskNotifyGive(Current_Task);
-            }
+            HAL_UART_Transmit(&huart1, (uint8_t*)"PAD:Ack\n", strlen("PAD:Ack\n"), 100);
         }
 
     }
@@ -98,6 +102,7 @@ extern "C" void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
                 uart1.rx_index = 0;
             }
         }
+        __HAL_UART_CLEAR_OREFLAG(huart);
 
         // 继续接收下一个字节
         HAL_UART_Receive_IT(&huart1, &uart1.rx_byte, 1);
@@ -105,3 +110,4 @@ extern "C" void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
         portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
     }
 }
+
